010c0378d09b4d1b7285e8c36322ea3a53f8d7ee
Maks Kozharin <maksko@yandex-team.ru>
2019-07-09 09:20:40 +0300

    modify comments -- will try new experiments

--- 004-py-nltk-test/text_processor.py
+++ 004-py-nltk-test/text_processor.py
@@ -20,7 +20,7 @@ class TextProcessor(object):
         self.stored_wordforms = db.get_all_wordforms()
         self.wordforms_in_process = set()
 
-    def process_text(self, text_id):
+    def find_sentence_parts(self, text_id):
         sentences = self.db.get_sentences(text_id)
 
         swfgramm_dict = {}
@@ -43,5 +43,30 @@ class TextProcessor(object):
                 # swfgramm_info[1].append((tokens[i], i, sentence_id))
 
         ooo = [(o_md5, o[0], o[1]) for o_md5, o in swfgramm_dict.items() if len(o[1]) > 1]
+
+        # пример того, что получается
+        # ---------------------------------------------------------------
+        # [02] = {tuple} <class 'tuple'>: ('6fb9fdc07741e213d758bb6c2239b661', ['как', 'же'], defaultdict(<class 'list'>, {('а', 0): [60, 384, 1231], ('знаю', 2): [647], ('помню', 2): [702], ('мертвый', 2): [1252]}))
+        #      0 = {str} '6fb9fdc07741e213d758bb6c2239b661'
+        #      1 = {list} <class 'list'>: ['как', 'же']
+        #      2 = {defaultdict} defaultdict(<class 'list'>, {('а', 0): [60, 384, 1231], ('знаю', 2): [647], ('помню', 2): [702], ('мертвый', 2): [1252]})
+        #       ('а', 0) (139736621942536) = {list} <class 'list'>: [60, 384, 1231]
+        #       ('знаю', 2) (139736616910536) = {list} <class 'list'>: [647]
+        #       ('помню', 2) (139736616384456) = {list} <class 'list'>: [702]
+        #       ('мертвый', 2) (139736611277064) = {list} <class 'list'>: [1252]
+        #       __len__ = {int} 4
+        #       default_factory = {type} <class 'list'>
+        #      __len__ = {int} 3
+        # ---------------------------------------------------------------
+        # тут:
+        #       1 = {list} <class 'list'>: ['как', 'же']
+        #           -- постоянная структура
+        #       ('а', 0) (139736621942536) = {list} <class 'list'>: [60, 384, 1231]
+        #           -- модификации (добавляется 'а' в позицию 0; полученное предложение встречается 3 раза; id предложений: 60, 384, 1231)
+        # т.е. алгоритм нашел следующие предложения:
+        #   а как же
+        #     как же, знаю
+        #     как же, помню
+        #     как же, мертвый
 
         pass
